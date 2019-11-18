a9c1bdeee83f5e7dcd5b64693ccdb31b4cd2ac5c
Miguel Muñoz <miguel.munoz@longportaviation.com>
2019-11-18 11:14:54 -0500

    [FIX] Fix errors in plugins.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -30,9 +30,9 @@
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-
+// import 'core-js/es6/reflect';
+// import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
@@ -59,7 +59,7 @@ import 'core-js/es7/reflect';
  * Zone JS is required by default for Angular itself.
  */
 // import 'zone.js/dist/zone'; // Included with Angular CLI.
-import 'zone.js'
+import 'zone.js';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
