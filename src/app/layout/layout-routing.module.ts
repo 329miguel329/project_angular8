b2c53440cf483d4ffca3f2148c0bc3c99de1828e
Miguel Muñoz <miguel.munoz@longportaviation.com>
2019-11-18 11:31:42 -0500

    [UPD] Update layout remved coments.

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -8,18 +8,10 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       { path: '', redirectTo: 'colegio', pathMatch: 'prefix' },
-      // { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
-    //   { path: 'charts', loadChildren: () => import('./charts/charts.module').then(m => m.ChartsModule) },
-    //   { path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
       { path: 'colegio', loadChildren: () => import('./colegio/colegio.module').then(m => m.ColegioModule) },
       { path: 'estadistica/:codigo', loadChildren: () => import('./estadistica/estadistica.module').then(m => m.EstadisticaModule) },
       { path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule) },
       { path: 'estudiante/:id', loadChildren: () => import('./estudiante/estudiante.module').then(m => m.EstudianteModule) }
-      // { path: 'forms', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
-      // { path: 'bs-element', loadChildren: () => import('./bs-element/bs-element.module').then(m => m.BsElementModule) },
-      // { path: 'grid', loadChildren: () => import('./grid/grid.module').then(m => m.GridModule) },
-      // { path: 'components', loadChildren: () => import('./bs-component/bs-component.module').then(m => m.BsComponentModule) },
-      // { path: 'blank-page', loadChildren: () => import('./blank-page/blank-page.module').then(m => m.BlankPageModule) }
     ]
   }
 ];
