7207ba4137c61fcbed5855bc289dfe7ead96833e
Angie Palacios <adsi2.co@longportaviation.com>
2019-11-19 00:08:58 -0500

    [DEL] Delete various content.

--- src/app/layout/colegio/colegio.component.ts
+++ src/app/layout/colegio/colegio.component.ts
@@ -5,19 +5,19 @@ import { routerTransition } from '../../router.animations';
 import { Router } from '@angular/router';
 
 class Colegio {
-    codigo: string;
-    nombre: string;
-    constructor(colegio: any) {
-        this.codigo = colegio.codigo;
-        this.nombre = colegio.nombre;
-    }
+  codigo: string;
+  nombre: string;
+  constructor (colegio: any) {
+    this.codigo = colegio.codigo;
+    this.nombre = colegio.nombre;
+  }
 }
 
 class DataTablesResponse {
-    data: Colegio[];
-    draw: number;
-    recordsFiltered: number;
-    recordsTotal: number;
+  data: Colegio[];
+  draw: number;
+  recordsFiltered: number;
+  recordsTotal: number;
 }
 
 @Component({
@@ -27,62 +27,62 @@ class DataTablesResponse {
   animations: [routerTransition()]
 })
 export class ColegioComponent implements OnInit, AfterViewInit {
-    dtOptions: DataTables.Settings = {};
-    colegios: Colegio[];
+  dtOptions: DataTables.Settings = {};
+  colegios: Colegio[];
 
-    constructor (
-        private http: HttpClient,
-        private servicioColegio: ServicioColegio,
-        private renderer: Renderer,
-        private router: Router) {
+  constructor (
+    private http: HttpClient,
+    private servicioColegio: ServicioColegio,
+    private renderer: Renderer,
+    private router: Router) {
 
+  }
+
+  ngOnInit() {
+    const that = this;
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 15,
+      serverSide: true,
+      processing: true,
+      autoWidth: false,
+      ajax: (dataTablesParameters: any, callback) => {
+        this.servicioColegio.obtenerColegios()
+          .valueChanges().subscribe(colegios => {
+            console.log(colegios);
+            that.colegios = colegios.map((colegio) => {
+              return new Colegio(colegio);
+            });
+            callback({
+              recordsTotal: colegios.length,
+              recordsFiltered: 1,
+              data: colegios.map((colegio) => {
+                return new Colegio(colegio);
+              })
+            });
+          });
+
+      },
+      columns: [
+        { data: 'nombre' },
+        { data: 'codigo' },
+        {
+          title: 'Action',
+          render: function (data: any, type: any, full: any) {
+            // tslint:disable-next-line:max-line-length
+            return '<a class="list-group-item btn" view-person-id="' + full.codigo + '"><li class="fa fa-eye"></li></a>';
+          }
         }
+      ]
+    };
+  }
 
-    ngOnInit() {
-        const that = this;
-
-        this.dtOptions = {
-            pagingType: 'full_numbers',
-            pageLength: 15,
-            serverSide: true,
-            processing: true,
-            autoWidth: false,
-            ajax: (dataTablesParameters: any, callback) => {
-                this.servicioColegio.obtenerColegios()
-                    .valueChanges().subscribe(colegios => {
-                        console.log(colegios);
-                        that.colegios = colegios.map((colegio) => {
-                            return new Colegio(colegio);
-                        });
-                        callback({
-                            recordsTotal: 50,
-                            recordsFiltered: 1,
-                            data: colegios.map((colegio) => {
-                                return new Colegio(colegio);
-                            })
-                        });
-                    });
-
-            },
-            columns: [
-                {data: 'nombre'},
-                {data: 'codigo'},
-                {
-                    title: 'Action',
-                    render: function (data: any, type: any, full: any) {
-                        // tslint:disable-next-line:max-line-length
-                        return '<a class="list-group-item btn" view-person-id="' + full.codigo + '"><li class="fa fa-eye"></li></a>';
-                    }
-                }
-            ]
-        };
-    }
-
-    ngAfterViewInit(): void {
-        this.renderer.listenGlobal('document', 'click', (event) => {
-            if (event.target.hasAttribute('view-person-id')) {
-                this.router.navigate(['/estadistica/' + event.target.getAttribute('view-person-id')]);
-            }
-        });
-    }
+  ngAfterViewInit(): void {
+    this.renderer.listenGlobal('document', 'click', (event) => {
+      if (event.target.hasAttribute('view-person-id')) {
+        this.router.navigate(['/estadistica/' + event.target.getAttribute('view-person-id')]);
+      }
+    });
+  }
 }
